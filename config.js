a116b9a459de9ab2a1d52bedd7df9d50d7580ca2
Amin Mahmoudi <lichwow_masterking@yahoo.com>
2024-04-22 05:47:12 +0330

    better structures!

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export const config = {
-  log_level: "debug", // Log Level: debug, info, warning, error
+  log_level: "info", // Log Level: debug, info, warning, error
   game_version: "3.3.5", // Game Version: 1.12.1, 2.4.3, 3.3.5 based on game version and packet structure
   build: 12340, // Build: 12340, ... based on game version and packet structure
   secret_key: "secret", // Secret Key
   auth_port: 3724, // Auth Port, default: 3724
   main_server_auth: {
-    host: "192.168.1.10", // Main Server Auth Host
+    host: "192.168.32.32", // Main Server Auth Host
     port: 3724, // Main Server Auth Port
   },
   realms: [
